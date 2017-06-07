37488488cd94f44a1bab677618e24e2053a882ec
Alex Bennett <kestralttr@gmail.com>
2017-06-07 13:25:53 -0700

    updates styling

--- javascript.js
+++ javascript.js
@@ -11,13 +11,10 @@
     inputValue = document.getElementById("input-field").value;
     if(inputValue !== "") {
       body.style.backgroundImage = "url(" + inputValue + ")";
+      body.style.backgroundSize = "cover";
+      document.getElementById("input-field").value = "";
     }
   });
 
-  return {
-    test: function() {
-      console.log("working");
-    }
-  };
 
 }());
